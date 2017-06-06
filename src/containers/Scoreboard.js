553dcf920cecdaf0631cccdc244ada4b8879f976
Chris Lawton <chris.lawton@gdsinternational.com>
2017-06-06 11:00:25 +0100

    adding add, remove and update score player actions but passing them to bind action creators so they will invoke and dispatch directly

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import * as PlayerActionCreators from '../actions/player';
 import AddPlayerForm from '../components/AddPlayerForm';
 import Player from '../components/Player';
 import Header from '../components/Header';
@@ -42,6 +44,10 @@ export default class Scoreboard extends Component{
   }
 
   render() {
+    const {dispatch, players} = this.props;
+    const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch);
+    const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch);
+    const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch);
     return (
       <div className="scoreboard">
         <Header players={this.state.players} />
